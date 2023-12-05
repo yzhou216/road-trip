868e7481241cf15beb06716907b91d08aa155886
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-12-05 01:47:06 -0800

    Add SateName.java

---
+++ src/main/java/io/yiyuzhou/trip/StateName.java
@@ -0,0 +1,45 @@
+package io.yiyuzhou.trip;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class StateName {
+	private HashMap<String, String> countries;
+
+	public StateName() {
+		countries = new HashMap<>();
+		parseStateNames();
+	}
+
+	public String getName(String id) {
+		return countries.get(id);
+	}
+
+	private void parseStateNames() {
+		try {
+			/* load the TSV file from the resources directory */
+			InputStream inputStream = StateName.class.getClassLoader().getResourceAsStream("state_name.tsv");
+			Reader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+			/* define the TSV format and create the parser */
+			CSVFormat format = CSVFormat.TDF.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim();
+			try (CSVParser csvParser = new CSVParser(reader, format)) {
+				for (CSVRecord record : csvParser) {
+					String stateId = record.get("stateid");
+					String countryName = record.get("countryname");
+					countries.put(stateId, countryName);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
