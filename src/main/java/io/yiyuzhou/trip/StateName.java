b683a2bd7725acd9888ddf25abfebe4eabe186f1
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-12-06 15:33:21 -0800

    Clean up StateName

--- src/main/java/io/yiyuzhou/trip/StateName.java
+++ src/main/java/io/yiyuzhou/trip/StateName.java
@@ -12,21 +12,15 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 public class StateName {
-	private HashMap<String, Integer> countriesIdNumbers;
-	private HashMap<String, String> countriesNameIds;
+	private HashMap<String, String> countryNameIds;
 
 	public StateName() {
-		countriesIdNumbers = new HashMap<>();
-		countriesNameIds = new HashMap<>();
+		countryNameIds = new HashMap<>();
 		parseStateNames();
 	}
 
-	public int getNumber(String id) {
-		return countriesIdNumbers.get(id);
-	}
-
 	public String getId(String countryName) {
-		return countriesNameIds.get(countryName);
+		return countryNameIds.get(countryName);
 	}
 
 	private void parseStateNames() {
@@ -41,11 +35,8 @@ public class StateName {
 				for (CSVRecord record : csvParser) {
 					String stateId = record.get("stateid");
 
-					String stateNumber = record.get("statenumber");
-					countriesIdNumbers.put(stateId, Integer.parseInt(stateNumber));
-
 					String countryName = record.get("countryname");
-					countriesNameIds.put(countryName, stateId);
+					countryNameIds.put(countryName, stateId);
 				}
 			}
 		} catch (Exception e) {
